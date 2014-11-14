7589c431294ad38adf1bcdc48eb931889f00fba6
DanSnow <dododavid006@gmail.com>
2014-11-14 15:47:19 +0800

    Update: Add admin user seed

--- app/services/create_admin_service.rb
+++ app/services/create_admin_service.rb
@@ -1,6 +1,9 @@
 class CreateAdminService
   def call
-    user = User.find_or_create_by!(email: Rails.application.secrets.admin_email) do |user|
+    user = User.find_or_create_by!(username: Rails.application.secrets.admin_username) do |user|
+        user.name = Rails.application.secrets.admin_username
+        user.role = :admin
+        user.email = Rails.application.secrets.admin_email
         user.password = Rails.application.secrets.admin_password
         user.password_confirmation = Rails.application.secrets.admin_password
       end
