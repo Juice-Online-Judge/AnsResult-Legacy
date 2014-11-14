8893bbe499d93edabd1079317ecd75bf59a156cb
DanSnow <dododavid006@gmail.com>
2014-11-14 14:30:58 +0800

    Update: Add user

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,6 +4,14 @@ class User < ActiveRecord::Base
   enum role: [:user, :admin]
   after_initialize :set_default_role, :if => :new_record?
 
+  def login=(login)
+    @login = login
+  end
+
+  def login
+    @login || self.username || self.email
+  end
+
   def set_default_role
     self.role ||= :user
   end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   root to: 'visitors#index'
   devise_for :users, :controllers => { :registrations => "devise/registrations" }
-  resource :list
+  resources :lists
+  resources :users
 end
