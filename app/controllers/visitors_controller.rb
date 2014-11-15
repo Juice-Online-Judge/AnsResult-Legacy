4b0f977296efa3d8128d26497767d0ab53d7ec7f
DanSnow <dododavid006@gmail.com>
2014-11-15 09:29:00 +0800

    Update: force to login

--- app/controllers/visitors_controller.rb
+++ app/controllers/visitors_controller.rb
@@ -2,6 +2,8 @@ class VisitorsController < ApplicationController
   def index
     if user_signed_in?
       redirect_to lists_url
+    else
+      redirect_to new_user_session_path
     end
   end
 end
