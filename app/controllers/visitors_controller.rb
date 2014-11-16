94df984ac9ec317cd97b5b0ad0fc42e6cd77b8bf
DanSnow <dododavid006@gmail.com>
2014-11-16 21:43:31 +0800

    Update: Change default display page

--- app/controllers/visitors_controller.rb
+++ app/controllers/visitors_controller.rb
@@ -1,7 +1,7 @@
 class VisitorsController < ApplicationController
   def index
     if user_signed_in?
-      redirect_to lists_url
+      redirect_to '/display'
     else
       redirect_to new_user_session_path
     end
